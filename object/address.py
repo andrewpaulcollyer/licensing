73d7e06106519debfb71b782db3c6db62f47e6c0
Andrew Collyer <andrewpaulcollyer@gmail.com>
2017-12-27 18:44:59 +0000

    Modified Address objects

--- object/address.py
+++ object/address.py
@@ -14,8 +14,10 @@ class AddressObject(db.Model):
     postcode = db.Column(db.String(80))
     country = db.Column(db.String(20))
     instructions = db.Column(db.String(80))
+    latitude = db.Column(db.Float(precision=7))
+    longitude = db.Column(db.Float(precision=7))
 
-    def __init__(self, address0, price, store_id):
+    def __init__(self, address1, rice, store_id):
         self.address1 = address1
         self.address2 = address2
         self.address3 = address3
@@ -25,9 +27,18 @@ class AddressObject(db.Model):
         self.country = country
         self.instructions = instructions
 
+    # Save Class Object to database
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
-    def method(self):
-        pass
+    def validate(self):
+        # Generate address string
+        address_string = self.address1 + " " + self.address2 + " " + self.address3 + " " + self.suburb + " " + self.state + " " + self.postcode + " " + self.country
+        # TODO alidate against google api
+        # https://developers.google.com/maps/documentation/javascript/places-autocomplete
+        validate = True
+        if validate:
+            return {'message': 'Address Validated'}
+        else:
+            return {'message':'Country not valid for application'}
