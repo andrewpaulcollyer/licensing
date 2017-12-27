dc3f30ebe3a611fa4266e657952324eb36dc38fe
Andrew Collyer <andrewpaulcollyer@gmail.com>
2017-12-27 13:50:13 +0000

    Added additional models / classes based on user stories

---
+++ object/item.py
@@ -0,0 +1,17 @@
+from db import db
+from flask import jsonify
+
+class ItemModel(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id):
+        self.name = name
+        self.price = price
+        self.store_id = store_id
